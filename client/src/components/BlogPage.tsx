c2f1e992aa5d9219756cff5750cba651b65e6855
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-10-24 10:27:45 +0100

    feat: Add loading state to BlogPage component

--- client/src/components/BlogPage.tsx
+++ client/src/components/BlogPage.tsx
@@ -5,15 +5,24 @@ import { formatRelativeTime } from '../utils';
 const BlogPage = () => {
     const { id } = useParams();
     const [blog, setBlog] = useState<Blog | null>(null);
-
+    const [isPending, setPending] = useState(true);
     useEffect(() => {
         fetch(`http://localhost:8000/posts/${id}`).then((resp) =>
             resp.json().then((data) => {
                 setBlog(data.data);
-                console.log(data);
+                setPending(false);
+                // console.log(data);
             })
         );
     }, [id]);
+
+    if (isPending) {
+        return (
+            <div className="flex justify-center items-center">
+                <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-900"></div>
+            </div>
+        );
+    }
 
     return (
         <div className="px-4 w-full max-w-5xl mx-auto bg-white mt-2 py-4">
